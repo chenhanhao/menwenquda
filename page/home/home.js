d5197b3761191e03e2c24789e1297e2bc4ca1c93
chh <477@qq.com>
2019-07-16 09:52:18 +0800

    第三次
    第三次

--- page/home/home.js
+++ page/home/home.js
@@ -14,6 +14,7 @@ Page({
   doPost(){
     console.log("----------")
     console.log("---------")
+    console.log("---------")
     // 2.使用封装的request发送网络请求
     // Promise最大的好处就是防止出现回调地狱
     request({
