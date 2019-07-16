c5dc33fd5d0f93d5f6c361f028438f8163e108c1
chh <477@qq.com>
2019-07-16 22:36:51 +0800

    第6次
    4154

--- page/home/home.js
+++ page/home/home.js
@@ -8,19 +8,64 @@ Page({
    * 页面的初始数据
    */
   data: {
-   
+     appScreat:'c4d418debb577ce68a11fa62a7f98bfe',
+     appId:'wx3b8b3392c97707d6'
+
   },
 
-  doPost(){
+  /*doPost(){
     console.log("----------")
-    console.log("---------")
-    console.log("---------")
+    console.log(this.appScreat)
     // 2.使用封装的request发送网络请求
     // Promise最大的好处就是防止出现回调地狱
     request({
-      url: 'http://192.168.56.1:8080/abc'
+      url: 'http://192.168.137.1:8080/abc'
+      
     }).then(res => {
       console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
+    
+  },*/
+
+
+  doPost(){
+    
+
+    // 2.使用封装的request发送网络请求
+    // Promise最大的好处就是防止出现回调地狱
+    request({
+     
+     /* url: 'http://192.168.137.1:8080/user/login',
+      method: 'post',
+      data: {
+        username: '123',
+        password: '123'
+      },
+      header:{
+        //浏览器的原生 <form> 表单,其中ajax也是用这种方式提交的
+        //让后台可以解析到正确数据
+        'content-type': 'application/x-www-form-urlencoded'
+      }*/
+      url: 'http://192.168.56.1:8080/ll',
+      method: 'post',
+      data: {
+        username: '123',
+        password: '123'
+      },
+      header:{
+        //浏览器的原生 <form> 表单,其中ajax也是用这种方式提交的
+        //让后台可以解析到正确数据
+        'content-type': 'application/x-www-form-urlencoded',
+        token:'1eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1NjMyODUxNTAsInVOYW1lIjoiMTIzIiwidUlkIjoiMTIzIiwiZXhwIjo0NTYzMjg1MTUwfQ.t_2nWZ_dkN9KypzPnYwgZ3NWVlJNybAn6DsvKefnnMg'
+      }
+
+
+      
+    }).then(res => {
+      console.log(res)
+      console.log(res.data.data)
     }).catch(err => {
       console.log(err)
     })
--- service/network.js
+++ service/network.js
@@ -1,9 +1,11 @@
 export default function request(options) {
+  console.log(options.data)
   return new Promise((resolve, reject) => {
     wx.request({
       url: options.url,
       method: options.method || 'get',
       data: options.data || {},
+      header:options.header,
       success: resolve,
       fail: reject
     })
